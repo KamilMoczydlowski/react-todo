cbd74c933845f4b5a932dde854cafb28d8309fd4
KamilMoczydlowski <kamil.moczydlowski@icloud.com>
2023-01-19 14:35:23 +0100

    routes updated to 6.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,91 @@
-import { useContext } from 'react';
+import { lazy, Suspense, useContext } from 'react';
 
-import { Route, Navigate, BrowserRouter, Routes } from 'react-router-dom';
+import {
+	createBrowserRouter,
+	RouterProvider,
+	Navigate,
+} from 'react-router-dom';
 
 import WelcomePage from './pages/WelcomePage';
 
-import MainPage from './pages/MainPage';
-import AddNewTaskPage from './pages/AddNewTaskPage';
-import AddNewCategoryPage from './pages/AddNewCategoryPage';
-import CategoryBigCardPage from './pages/CategoryBigCardPage';
-import AddColorsAndIconsPage from './pages/AddColorsAndIconsPage';
-
 import AuthContext from './store/auth-context';
+
+const MainPage = lazy(() => import('./pages/MainPage'));
+const AddNewCategoryPage = lazy(() => import('./pages/AddNewCategoryPage'));
+const AddNewTaskPage = lazy(() => import('./pages/AddNewTaskPage'));
+const CategoryBigCardPage = lazy(() => import('./pages/CategoryBigCardPage'));
+const AddColorsAndIconsPage = lazy(() =>
+	import('./pages/AddColorsAndIconsPage')
+);
+
+const router = authCtx =>
+	createBrowserRouter([
+		{
+			path: '/auth',
+			element: authCtx.isLoggedIn ? (
+				<Navigate replace to='/main' />
+			) : (
+				<WelcomePage />
+			),
+		},
+		{
+			path: '/main',
+			element: authCtx.isLoggedIn ? (
+				<Suspense fallback={<p>Loading...</p>}>
+					<MainPage />
+				</Suspense>
+			) : (
+				<Navigate replace to='/auth' />
+			),
+		},
+		{
+			path: '/main/:id',
+			element: authCtx.isLoggedIn ? (
+				<Suspense fallback={<p>Loading...</p>}>
+					<CategoryBigCardPage />
+				</Suspense>
+			) : (
+				<Navigate replace to='/auth' />
+			),
+		},
+		{
+			path: '/addNewTask',
+			element: authCtx.isLoggedIn ? (
+				<Suspense fallback={<p>Loading...</p>}>
+					<AddNewTaskPage />
+				</Suspense>
+			) : (
+				<Navigate replace to='/auth' />
+			),
+		},
+		{
+			path: '/addNewCategory',
+			element: authCtx.isLoggedIn ? (
+				<Suspense fallback={<p>Loading...</p>}>
+					<AddNewCategoryPage />
+				</Suspense>
+			) : (
+				<Navigate replace to='/auth' />
+			),
+		},
+		{
+			path: '/addColorsAndIcons',
+			element: (
+				<Suspense fallback={<p>Loading...</p>}>
+					<AddColorsAndIconsPage />
+				</Suspense>
+			),
+		},
+		{
+			path: '/*',
+			element: <Navigate replace to='/auth' />,
+		},
+	]);
 
 function App() {
 	const authCtx = useContext(AuthContext);
 
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route
-					path='/auth'
-					element={
-						authCtx.isLoggedIn ? (
-							<Navigate replace to='/main' />
-						) : (
-							<WelcomePage />
-						)
-					}
-				/>
-				<Route
-					path='/main'
-					element={
-						authCtx.isLoggedIn ? <MainPage /> : <Navigate replace to='/auth' />
-					}
-				/>
-				<Route
-					path='/main/:id'
-					element={
-						authCtx.isLoggedIn ? (
-							<CategoryBigCardPage />
-						) : (
-							<Navigate replace to='/auth' />
-						)
-					}
-				/>
-				<Route
-					path='/addNewTask'
-					element={
-						authCtx.isLoggedIn ? (
-							<AddNewTaskPage />
-						) : (
-							<Navigate replace to='/auth' />
-						)
-					}
-				/>
-				<Route
-					path='/addNewCategory'
-					element={
-						authCtx.isLoggedIn ? (
-							<AddNewCategoryPage />
-						) : (
-							<Navigate replace to='/auth' />
-						)
-					}
-				/>
-				<Route path='/addColorsAndIcons' element={<AddColorsAndIconsPage />} />
-				<Route path='/*' element={<Navigate replace to='/auth' />} />
-			</Routes>
-		</BrowserRouter>
-	);
+	return <RouterProvider router={router(authCtx)} />;
 }
 
 export default App;
